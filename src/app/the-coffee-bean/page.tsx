5ca6021b63703b58c0ed98dd0604a227f8d8cd72
charles <charles@charless-MacBook-Pro.local>
2024-03-23 19:27:56 +1100

    google and apple store btn component

--- src/app/the-coffee-bean/page.tsx
+++ src/app/the-coffee-bean/page.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 import { PageProps } from '@/lib/interfaces'
 
+import HeroSection from '@/components/shared/HeroSection'
+import { FlexContainer } from '@/components/shared/layout/FlexContainer'
 import RootContainer from '@/components/shared/layout/RootContainer'
-import { TypographyH1 } from '@/components/shared/Typography/Typography'
+import AppleStoreBtn from '@/components/shared/LinkButton/AppleStoreBtn'
+import GooglePlayBtn from '@/components/shared/LinkButton/GooglePlayBtn'
+import { TypographyH1,TypographyH2 } from '@/components/shared/Typography/Typography'
 
 import TitleWrapper from './components/TitleWrapper'
-import HeroSection from '@/components/shared/HeroSection'
 
 const ContactPage: React.FC<PageProps> = () => {
   return (
@@ -14,8 +17,15 @@ const ContactPage: React.FC<PageProps> = () => {
       <TitleWrapper>
         <TypographyH1> The coffee bean</TypographyH1>
       </TitleWrapper>
+
       <HeroSection filepath='/next.svg' image_description='the-coffee-bean-icon'>
-        Connecting Over Coffee <br /> Networking in the Digital Age
+        <TypographyH2>Connecting Over Coffee</TypographyH2>
+        <br />
+        <TypographyH2>Networking in the Digital Age</TypographyH2>
+        <FlexContainer classname='mt-16 gap-10'>
+          <AppleStoreBtn href='http://google.com' />
+          <GooglePlayBtn href='http://google.com' />
+        </FlexContainer>
       </HeroSection>
     </RootContainer>
   )
--- src/components/shared/LinkButton/AppleStoreBtn/AppleStoreBtn.tsx
+++ src/components/shared/LinkButton/AppleStoreBtn/AppleStoreBtn.tsx
@@ -1,7 +1,19 @@
+import Link from 'next/link'
 import React from 'react'
+import { FaApple } from 'react-icons/fa6'
 
-const AppleStoreBtn = () => {
-  return <div>AppleStoreButton</div>
+const AppleStoreBtn = ({ href }: { href: string }) => {
+  return (
+    <Link href={href}>
+      <div className='flex bg-black text-white w-[200px] max-w-[250px] max-h-[50px] rounded-md py-3 px-2 gap-3 overflow-hidden'>
+        <FaApple className='w-1/5 h-auto' />
+        <div className='text-center flex flex-col justify-center items-center '>
+          <div>Download on the </div>
+          <div className=' font-thin text-xl'>App Store</div>
+        </div>
+      </div>
+    </Link>
+  )
 }
 
 export default AppleStoreBtn
