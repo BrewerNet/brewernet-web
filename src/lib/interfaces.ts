8ba7846ab75a7edc65561645d0d4191b20fc6865
charles <charles@charless-MacBook-Pro.local>
2024-03-23 13:53:06 +1100

    add useful container component and constant.ts

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -2,7 +2,10 @@ import { ReactNode } from 'react'
 
 export interface PageProps {}
 
-export interface TypographyProps {
+export interface ContainerProps {
   children: ReactNode
+}
+
+export interface TypographyProps extends ContainerProps {
   classname?: string
 }
