f962b618c26e2dea845fa5e9c2ed4fa0a78e17f8
V1ncentNg <112238267+V1ncentNg@users.noreply.github.com>
2024-03-24 21:20:24 +1100

    finish first version nav bar

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -1,21 +1,51 @@
+"use client";
+
 import Image from 'next/image'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styles from './Navbar.module.css'
+import { LuMenu } from "react-icons/lu";
+import { RxCross1 } from "react-icons/rx";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      console.log(open);
+      if(window.innerWidth > 768) setOpen(false);
+    };
+    
+    window.addEventListener('resize', handleResize);
+    handleResize(); 
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className= "h-[10%] w-full flex flex-row items-center justify-between px-2 py-1 ">
-      <div className = "h-auto w-fit flex flex-row items-center">
-        <Image src = "/BrewernetIcon.png" alt = "IconImage" width={75} height={75}></Image>
-        <div className='text-lg font-bold pb-1'>Brewernet</div>
-      </div>
-      <div className = "w-[40%] h-fit flex flex-row items-center justify-evenly">
-        <div className='w-[70%] h-fit flex flex-row justify-evenly font-[Mitr] font-semibold'>
-          <div>Buy Me a Coffee</div>
-          <div>The Coffee Bean</div>
+    <>
+      <div className={styles.navBar}>
+        <div className = "h-auto w-fit flex flex-row items-center">
+          <Image src = "/BrewernetIcon.png" alt = "IconImage" width={75} height={75}></Image>
+          <div className='text-lg font-bold pb-1'>Brewernet</div>
         </div>
-        <Image src = "/ConnectButton.png" alt = "connectButton"  width = {200} height={75} className='w-fit h-[35px]'/>
+        <div className = {styles.buttonWrapper}>
+          <div className={styles.buttons}>
+            <div className='px-1'>Buy Me a Coffee</div>
+            <div className='px-1'>The Coffee Bean</div>
+          </div>
+          <Image src = "/ConnectButton.png" alt = "connectButton"  width = {200} height={75} className='w-fit h-[35px] '/>
+          <div className='md:hidden' onClick={() => setOpen(!open)}><LuMenu className='h-[35px] w-[35px]'/></div>
+        </div>
       </div>
-    </div>
+      {open && 
+      <div className={styles.menu}>
+          <RxCross1 onClick={() => setOpen(false)} className='w-[35px] h-[35px] absolute right-10 top-10'/>
+          <div className='p-5 font-[Mitr] font-semibold'>Buy Me a Coffee</div>
+          <div className='p-5 font-[Mitr] font-semibold'>The Coffee Bean</div>
+      </div>
+      }
+    </>
   )
 }
