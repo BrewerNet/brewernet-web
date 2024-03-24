011082735261f271f94a93e36dbdb12d1f98f477
V1ncentNg <112238267+V1ncentNg@users.noreply.github.com>
2024-03-24 22:24:08 +1100

    Finish hero section

---
+++ src/app/Hero/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './Hero.module.css'
+import Image from 'next/image'
+
+export default function Hero() {
+  return (
+    <div className={styles.heroSection}>
+      <div className={styles.textContainer}>
+        <div className={styles.name}>BrewerNet</div>
+        <div className={styles.slogan}>Connections Make the World Better</div>
+        <p className={styles.description}>A Community uses compassionate tech for innovative life improvements and a brighter future</p>
+      </div>
+      <Image src="/HeroBackground.png" alt="Hero Background" width={500} height={500} className={styles.heroImg}></Image>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import Image from 'next/image'
 import Navbar from './Navbar/Navbar'
+import Hero from './Hero/Hero'
 
 export default function Page() {
   return (
-    <Navbar/>
+    <>
+      <Navbar/>
+      <Hero/>
+    </>
+    
   )
 }
