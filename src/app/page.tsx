e6204ca56bcc9305dbf4ccf79da8f2ac2948e2cf
charles <charles@charless-MacBook-Pro.local>
2024-03-25 00:01:25 +1100

    refactor: change NavBar to layout and move Hero to component folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@ import Link from 'next/link'
 
 import { PageProps } from '@/lib/interfaces'
 
-import Hero from './Hero/Hero'
-import Navbar from './Navbar/Navbar'
+import Hero from '../components/shared/Hero/Hero'
 
 const Page: React.FC<PageProps> = () => {
   return (
     <div>
-      <Navbar />
       <Hero />
       <h1>Home</h1>
       <Link href='/about'>About</Link>
