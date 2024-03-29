e001791bf1f0c47b30e2dff4c2422b7a8a54d688
CharlesZz <106577352+CharlesZhao27@users.noreply.github.com>
2024-03-30 09:00:59 +1100

    remove unused variable in WhatYouCanDoSubSection.tsx

--- src/components/shared/LinkButton/DiscordBtn/DiscordBtn.tsx
+++ src/components/shared/LinkButton/DiscordBtn/DiscordBtn.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import Link from 'next/link'
+import React from 'react'
 import { IoLogoDiscord } from "react-icons/io5";
 
 const DiscordBtn= ({ href }: { href: string }) => {
