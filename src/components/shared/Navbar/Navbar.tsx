1cbe3b66496b80da5721f170c4b3afa41ee8ab7f
V1ncentNg <112238267+V1ncentNg@users.noreply.github.com>
2024-03-29 15:25:53 +1100

    fix nav bar and footer

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import { LuMenu } from 'react-icons/lu'
 import { RxCross1 } from 'react-icons/rx'
-
+import Link from 'next/link'
 import styles from './Navbar.module.css'
 
 export default function Navbar() {
@@ -28,13 +28,21 @@ export default function Navbar() {
     <>
       <div className={styles.navBar}>
         <div className='h-auto w-fit flex flex-row items-center'>
-          <Image src='/BrewernetIcon.png' alt='IconImage' width={75} height={75}></Image>
-          <div className='text-lg font-bold pb-1'>Brewernet</div>
+          <Link href="/homepage">
+            <Image src='/BrewernetIcon.png' alt='IconImage' width={75} height={75}></Image>
+          </Link>
+          <Link href="/homepage">
+            <div className='text-lg font-bold pb-1'>Brewernet</div>
+          </Link>
         </div>
         <div className={styles.buttonWrapper}>
           <div className={styles.buttons}>
+          <Link href="/buy-me-a-coffee">
             <div className='px-1'>Buy Me a Coffee</div>
+          </Link>
+          <Link href="/the-coffee-bean">
             <div className='px-1'>The Coffee Bean</div>
+          </Link>         
           </div>
           <Image src='/ConnectButton.png' alt='connectButton' width={200} height={75} className='w-fit h-[35px] ' />
           <div className='md:hidden' onClick={() => setOpen(!open)}>
@@ -45,8 +53,12 @@ export default function Navbar() {
       {open && (
         <div className={styles.menu}>
           <RxCross1 onClick={() => setOpen(false)} className='w-[35px] h-[35px] absolute right-10 top-10' />
-          <div className='p-5 font-[Mitr] font-semibold'>Buy Me a Coffee</div>
-          <div className='p-5 font-[Mitr] font-semibold'>The Coffee Bean</div>
+          <Link href="/buy-me-a-coffee">
+            <div className='p-5 font-[Mitr] font-semibold'>Buy Me a Coffee</div>
+          </Link>
+          <Link href="/the-coffee-bean">
+            <div className='p-5 font-[Mitr] font-semibold'>The Coffee Bean</div>
+          </Link>
         </div>
       )}
     </>
