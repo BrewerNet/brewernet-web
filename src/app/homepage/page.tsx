d714f792d703e3045c81abdf07b1dacc4fb9a486
Ocean <zouhaiyang2005@gmail.com>
2024-04-04 20:29:47 +1100

    add link to homepage button, create homepageData file to config homepage row data

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -5,46 +5,13 @@ import { PageProps } from '@/lib/interfaces'
 
 import CardContainer from './components/CardContainer'
 import TeamSection from './components/TeamSection'
+import { cardData, teamData } from './homepageData'
 import { FlexContainer } from '../../components/shared/layout/FlexContainer'
 import { TypographyH1, TypographyH2, TypographyLarge } from '../../components/shared/Typography/Typography'
 import banner from '../../../public/banner.svg'
-import btcbFaviconC from '../../../public/logo/bmac-favicon-c.png'
-import tcbFaviconC from '../../../public/logo/tcb-favicon-c.png'
-import placeholderImage from '../../../public/placeholderImage.png'
+
 const Homepage: React.FC<PageProps> = () => {
-  const teamData = [
-    {
-      title: 'Marketing',
-      images: [
-        { src: placeholderImage, alt: 'Marketing Image 1' },
-        { src: placeholderImage, alt: 'Marketing Image 2' },
-        { src: placeholderImage, alt: 'Marketing Image 3' }
-      ]
-    },
-    {
-      title: 'Research',
-      images: [
-        { src: placeholderImage, alt: 'Research Image 1' },
-        { src: placeholderImage, alt: 'Research Image 2' },
-        { src: placeholderImage, alt: 'Research Image 2' }
-      ]
-    },
-    {
-      title: 'IT',
-      images: [
-        { src: placeholderImage, alt: 'IT Image 1' },
-        { src: placeholderImage, alt: 'IT Image 2' },
-        { src: placeholderImage, alt: 'IT Image 2' }
-      ]
-    },
-    {
-      title: 'Design',
-      images: [
-        { src: placeholderImage, alt: 'Design Image 1' },
-        { src: placeholderImage, alt: 'Design Image 2' }
-      ]
-    }
-  ]
+
   return (
     <div>
       <div className='mb-24'>
@@ -62,23 +29,18 @@ const Homepage: React.FC<PageProps> = () => {
       <FlexContainer>
         <TypographyH1>Product</TypographyH1>
       </FlexContainer>
-
+      {cardData.map((card, index) => (
       <CardContainer
-        title='The Coffee Bean'
-        description='The Coffee Bean is a social networking community on Discord created to provide students with a space to discuss homework, share campus life, and connect with teammates for academic projects, side projects, or startup ventures. This server is completely free and open to students from all educational institutions.'
-        imageSrc={tcbFaviconC}
-        altText='tcb-favicon-c'
-        buttonContent='about TCB'
-        isImageOnRight={false}
+        key={index}
+        title={card.title}
+        description={card.description}
+        imageSrc={card.imageSrc}
+        altText={card.altText}
+        buttonContent={card.buttonContent}
+        linkHref={card.linkHref}
+        isImageOnRight={card.isImageOnRight}
       />
-      <CardContainer
-        title='Buy Me a Coffee'
-        description='Buy Me a Coffee is an innovative platform designed to revolutionize networking by blending digital connections with real-world interactions. Our goal is to encourage individuals to engage in meaningful conversations over coffee, fostering genuine relationships and knowledge sharing.'
-        imageSrc={btcbFaviconC}
-        altText='btcb-favicon-c'
-        buttonContent='about BMac'
-        isImageOnRight={true}
-      />
+    ))}
       <FlexContainer classname='mb-16 '>
         <TypographyH1>Team</TypographyH1>
       </FlexContainer>
